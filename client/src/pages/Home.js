2745a9338ebb1ccfb372967a9ef3df4e9f9cd509
DEVfancybear <duonghacker2345@gmail.com>
2020-05-04 15:39:33 +0700

    update component Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import Contacts from '../components/contacts/Contacts';
+import ContactForm from '../components/contacts/ContactForm';
+import ContactFilter from '../components/contacts/ContactFIlter';
+
 const Home = () => {
     return (
-        <div>Home</div>
-    )
-}
+        <div className='grid-2'>
+            <div>
+                <ContactForm/>
+            </div>
+            <div>
+                <ContactFilter/>
+                <Contacts/>
+            </div>
+        </div>
+    );
+};
+
 export default Home;
