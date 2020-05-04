0c1d6f39b4a693c41ed4500c9966faad07d8dfe6
DEVfancybear <duonghacker2345@gmail.com>
2020-05-04 11:28:00 +0700

    setting ui and theme project, create folder components and Navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = ({title, icon}) => {
+    return (
+        <div className='navbar bg-primary'>
+            <h1>
+                <i className={icon}/> {title}
+            </h1>
+        </div>
+    )
+}
+Navbar.defaultProps = {
+    title: "Contact Keeper",
+    icon: "fas fa-id-card-alt"
+}
+export default Navbar;
