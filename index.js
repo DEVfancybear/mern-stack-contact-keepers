a8af97ae9774a7bda985cfff1afdef57fd5991b0
DEVfancybear <duonghacker2345@gmail.com>
2020-05-04 13:42:40 +0700

    export ActionTypes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 connectDB();
 app.use(cors());
@@ -13,5 +15,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/contacts", require("./routes/api/contacts"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    //
+    app.get("*", (req, res) => {
+        res.sendfile(path.join((__dirname = "client/build/index.html")));
+    });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
