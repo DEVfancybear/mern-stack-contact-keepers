6aee3a69ac472a25b7e47c9a54991f34de661521
DEVfancybear <duonghacker2345@gmail.com>
2020-05-04 10:20:42 +0700

    connect mongodb to our app

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 const app = express();
+connectDB();
+app.use(cors());
 app.get("/", (req, res) => {
     res.json({msg: "Welcome to Backend API"})
 })
