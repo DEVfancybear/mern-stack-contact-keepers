57ad6ce4e27261c314d89e483f3c7ce869df05e5
DEVfancybear <duonghacker2345@gmail.com>
2020-05-04 11:41:05 +0700

    config redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducers from "./reducers/index";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from "react-redux";
 
+const store = createStore(
+    rootReducers, composeWithDevTools(applyMiddleware(thunk))
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider stoe={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
